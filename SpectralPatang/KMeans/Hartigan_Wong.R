4ba66874402cbd382af8f7057c98fc5f266aa514
patang <patrickangst@gmail.com>
2024-12-11 14:04:07 +0100

    Update Hartigan_Wong.R

--- SpectralPatang/KMeans/Hartigan_Wong.R
+++ SpectralPatang/KMeans/Hartigan_Wong.R
@@ -10,7 +10,7 @@ library(stats)
 library(NbClust)
 
 ## Patang test
-pca_hs_image_path <- '~/GitHub/UWW200_Master_Thesis_public/SpectralPatang/test_elbow_method/ang20180729t212542rfl/result/ang20180729t212542_rfl_v2r2_img_rectified/SPCA/PCA/OutputPCA_30_PCs'
+pca_hs_image_path <- '~/Documents/GitHub/UWW200_Master_Thesis_public/SpectralPatang/test_data_elbow/ang20180729t212542rfl/result/ang20180729t212542_rfl_v2r2_img_rectified/SPCA/PCA/OutputPCA_30_PCs'
 pca_hs_image <- terra::rast(pca_hs_image_path)
 num_cores <- parallel::detectCores()
 
@@ -18,7 +18,7 @@ num_cores <- parallel::detectCores()
 pca_hs_image_subset <- terra::subset(pca_hs_image, 1:6)
 
 # Downsample the raster (reduce spatial resolution)
-pca_hs_image_subset <- terra::aggregate(pca_hs_image_subset, fact = 16, fun = mean, cores = num_cores)
+pca_hs_image_subset <- terra::aggregate(pca_hs_image_subset, fact = 1, fun = mean, cores = num_cores)
 
 # read values as a matrix
 pca_data_convex <- as.matrix(terra::values(pca_hs_image_subset))
